dd978d73e0fd61721558f4156b31729b4ccbd13d
Michael De Madet <michaeldemadet@gmail.com>
2019-12-05 10:24:16 +0100

    first commit

---
+++ src/main/java/fr/formation/loansimulator/LoansimulatorApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.loansimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/formation/loansimulator/LoansimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.loansimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
